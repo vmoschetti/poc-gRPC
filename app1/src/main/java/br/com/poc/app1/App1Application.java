85d0dac7742dab3052417c486aba47fdcedacfeb
vmoschetti <vmoschetti@uolinc.com>
2024-11-08 15:33:18 -0300

    fluxo inicial para gerar registros mockados e consultar

--- app1/src/main/java/br/com/poc/app1/App1Application.java
+++ app1/src/main/java/br/com/poc/app1/App1Application.java
@@ -1,13 +1,25 @@
 package br.com.poc.app1;
 
+import br.com.poc.app1.coreH2.MockerService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App1Application {
 
+	@Autowired
+	public MockerService mockerService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
+	}
+
+
+	@PostConstruct
+	public void init() {
+		mockerService.loadTable();
 	}
 
 }
--- app2/src/main/java/br/com/poc/app2/App2Application.java
+++ app2/src/main/java/br/com/poc/app2/App2Application.java
@@ -1,13 +1,25 @@
 package br.com.poc.app2;
 
+import br.com.poc.app2.coreH2.MockerService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App2Application {
 
+    @Autowired
+    public MockerService mockerService;
+
     public static void main(String[] args) {
         SpringApplication.run(App2Application.class, args);
+    }
+
+
+    @PostConstruct
+    public void init() {
+        mockerService.loadTable();
     }
 
 }
